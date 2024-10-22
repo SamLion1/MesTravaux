89bf4d88ee4e475e6ffa12b8175c11eaff017578
SamLion1 <morpheus9@free.fr>
2024-10-22 11:28:26 +0200

    Bug

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,6 +1,8 @@
+<?php
+
 namespace App\Controller;
 
-use App\Form\InscriptionType; // Correction : Assurez-vous d'importer la bonne classe de formulaire
+use App\Form\InscriptionType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -8,31 +10,36 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-#[Route('/home', name: 'app_home')]
-public function form(Request $request): Response
-{
-// Correction : Utiliser InscriptionType si c'est le nom de votre formulaire
-$form = $this->createForm(InscriptionType::class);
+    #[Route('/home', name: 'app_home')]
+    public function form(Request $request): Response
+    {
+        // Utilisation d'InscriptionType pour créer le formulaire
+        $form = $this->createForm(InscriptionType::class);
 
-$form->handleRequest($request);
+        // Gestion de la requête du formulaire
+        $form->handleRequest($request);
 
-if ($form->isSubmitted() && $form->isValid()) {
-// Traiter les données du formulaire ici
+        // Si le formulaire est soumis et valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Traitez les données du formulaire ici, par exemple :
+            // $data = $form->getData();
 
-// Par exemple, rediriger vers une autre page
-return $this->redirectToRoute('app_home');
-}
+            // Redirection après soumission
+            return $this->redirectToRoute('app_home');
+        }
 
-return $this->render('home.html.twig', [
-'form' => $form->createView(),
-]);
-}
+        // Rendu du template avec la vue du formulaire
+        return $this->render('home.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
 
-#[Route('/home/index', name: 'app_home_index')] // Ajout de la route pour la méthode index
-public function index(): Response
-{
-return $this->render('home/index.html.twig', [
-'controller_name' => 'HomeController',
-]);
-}
+    #[Route('/home/index', name: 'app_home_index')]
+    public function index(): Response
+    {
+        // Rendu du template pour la méthode index
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
 }
